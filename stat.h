f47a5aae21447d21a650a1a1fcbd7acb47472a9c
Sarah Jamie Lewis <me@sarahjamielewis.com>
2015-03-19 20:20:36 -0700

    Formatting and chmod.

--- stat.h
+++ stat.h
@@ -3,12 +3,12 @@
 #define T_DEV  3   // Device
 
 struct stat {
-  short type;  // Type of file
-  int dev;     // File system's disk device
-  uint ino;    // Inode number
-  short nlink; // Number of links to file
-  short ownerid; // The owner of the file
-  short groupid; // The group owner of the file
-  uint mode;     // The permissions mode of the file
-  uint size;   // Size of file in bytes
+    short type;  // Type of file
+    int dev;     // File system's disk device
+    uint ino;    // Inode number
+    short nlink; // Number of links to file
+    short ownerid; // The owner of the file
+    short groupid; // The group owner of the file
+    uint mode;     // The permissions mode of the file
+    uint size;   // Size of file in bytes
 };
--- user.h
+++ user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int chmod(char*, int);
 
 // ulib.c
 int stat(char*, struct stat*);
